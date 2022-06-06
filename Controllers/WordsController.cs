665cc98b5319f787d38bedb67627994a01f6145f
Ataur Rahaman <mh5207270@gmail.com>
2022-06-06 20:21:31 +0600

    Add project files.

---
+++ Controllers/WordsController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WordsController : Controller
+    {
+        public static int LineBreak;
+        private static List<String> Words = new List<String>{};
+        [HttpGet]
+        public async Task<ActionResult<string>> Get()
+        {
+            String Output = "", Answer = "", End = "";
+            for (int i = 0; i < Words.Count; i++)
+            {
+                Output += Words[i];
+            }
+            for (int i = 0; i < Output.Length; i++)
+            {
+                if (i % LineBreak == 0 && i != 0)
+                {
+                    End = "";
+                    string Temp = Output.Substring(i - LineBreak, LineBreak);
+                    Temp += "\r\n";
+                    Answer += Temp;
+                }
+                End += Output[i];
+            }
+            if (End.Length > 0) Answer += End;
+            return Ok(Answer);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<WordListModel>>> Add([FromHeader(Name = "page-size")] int PageSize, WordListModel wordList)
+        {
+            foreach (var i in wordList.Words)
+            {
+                Words.Add(i);
+            }
+            LineBreak = PageSize;
+            return Created("~api/MyApi", wordList);
+        }
+
+    }
+}
---
+++ WordListModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyApi
+{
+    public class WordListModel
+    {
+        public List<string> Words { get; set; }
+    }
+}
